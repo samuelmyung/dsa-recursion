6ff9433e856a3bdf5b23581bf0451582d681956c
Samuel Myung <shmyung4@gmail.com>
2024-04-23 13:32:22 -0700

    Finished first 2 problems

--- recursion/every-other/everyOther.ts
+++ recursion/every-other/everyOther.ts
@@ -1,7 +1,12 @@
 /** everyOther: return a string with every other letter. */
 
 function everyOther(str: string): string {
-  return "x";
+  if (str.length === 0){
+    debugger;
+    return "";
+  }
+  debugger;
+  return str[0] + everyOther(str.slice(2))
 }
 
 export { everyOther };
--- recursion/product/product.ts
+++ recursion/product/product.ts
@@ -3,7 +3,11 @@ import { LLNodeNum } from "../common/ll";
 /** product: calculate the product of a linked list of numbers. */
 
 function product(nums: LLNodeNum | null): number {
-  return 42;
+  if (!nums){
+    return 1;
+  }
+
+  return nums!.val * product(nums!.next)
 }
 
 export { product };
