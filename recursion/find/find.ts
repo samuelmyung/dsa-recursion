416e32af23706bcd9350ce0f95a9da779de890cf
Jazz Cheema <jaskamalc@gmail.com>
2024-04-23 14:35:32 -0700

    finished up till find

--- recursion/find/find.ts
+++ recursion/find/find.ts
@@ -1,7 +1,18 @@
 /** find: return t/f is val is in arr. */
 
+// [1,2,3]
+// 1 -> val?
+
+
+
 function find(arr: number[], val: number): boolean {
-  return true;
+  if (arr.length === 0) return false;
+
+    if(arr[0] === val){
+      return true
+  }
+
+  return find(arr.slice(1), val)
 }
 
 export { find };
--- recursion/longest/longest.ts
+++ recursion/longest/longest.ts
@@ -2,8 +2,15 @@ import { LLNodeStr } from "../common/ll";
 
 /** longest: return length of longest word in a linked list of words. */
 
+//store length prop (5) ^
+//length === highest    v
+
 function longest(words: LLNodeStr | null): number {
-  return 42;
+  if (!words) {
+    return 0;
+  }
+
+  return Math.max(words.val.length, longest(words.next));
 }
 
 export { longest };
