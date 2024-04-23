81ab9fff62920f59f0bbffc70e6767fba7799571
Samuel Myung <shmyung4@gmail.com>
2024-04-23 15:31:29 -0700

    Finished 2 problems

--- recursion/is-palindrome/isPalindrome.ts
+++ recursion/is-palindrome/isPalindrome.ts
@@ -1,7 +1,11 @@
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
 function isPalindrome(str: string): boolean {
-  return true;
+  if (str[0] !== str[str.length-1]) return false;
+  if (str.length < 2) return true;
+
+  return isPalindrome(str.slice(1,-1));
 }
 
 export { isPalindrome };
+
--- recursion/rev-string/revString.ts
+++ recursion/rev-string/revString.ts
@@ -1,7 +1,8 @@
 /** revString: return a copy of a string, but in reverse. */
 
 function revString(str: string): string {
-  return "x";
+  if (str.length === 0) return "";
+  return revString(str.slice(1)) + str[0];
 }
 
 export { revString };
