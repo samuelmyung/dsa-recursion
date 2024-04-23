d6783b516d271ed80d55eaa6e91a6d8c65e28ce5
Jazz Cheema <jaskamalc@gmail.com>
2024-04-23 16:36:22 -0700

    finished exercise

--- recursion/binary-search/binarySearch.ts
+++ recursion/binary-search/binarySearch.ts
@@ -1,8 +1,27 @@
 /** binarySearch: given a sorted array of numbers, and a value,
  * return true if val is in array, false if not present). */
 
-function binarySearch(arr: number[], val: number, left = 0, right = arr.length): boolean {
-  return false;
+function binarySearch(
+  arr: number[],
+  val: number,
+  left = 0,
+  right = arr.length
+): boolean {
+  if (left > right) {
+    return false;
+  }
+
+  const mid = Math.floor((right + left) / 2);
+
+  if (arr[mid] === val) {
+    return true;
+  }
+
+  if (arr[mid] > val) {
+    return binarySearch(arr, val, left, mid - 1);
+  }
+
+  return binarySearch(arr, val, mid + 1, right);
 }
 
 export { binarySearch };
--- recursion/find-index/findIndex.ts
+++ recursion/find-index/findIndex.ts
@@ -1,7 +1,22 @@
 /** findIndex: return index of val in arr (or -1 if val is not present). */
 
+//return index, or -1
+
+//arr -> [1, 2, 3], val = 0
+// -1 ->
+
 function findIndex(arr: number[], val: number): number {
-  return 42;
+  if (arr.length === 0) {
+    return -1;
+  }
+
+  if (arr[0] === val) {
+    return 0;
+  }
+
+  return findIndex(arr.slice(1), val) === -1
+    ? -1
+    : findIndex(arr.slice(1), val) + 1;
 }
 
 export { findIndex };
